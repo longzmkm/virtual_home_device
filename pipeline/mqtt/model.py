d19177189c7f4daafefbadc75fd2b7f95f8c10a5
Your Name <you@example.com>
2020-09-04 16:19:24 +0800

    f

--- pipeline/mqtt/model.py
+++ pipeline/mqtt/model.py
@@ -1,13 +1,40 @@
 # -*- coding: utf-8 -*-
 # © 2016 QYT Technology
 # Authored by: Liu tianlong (tlzmkm@gmail.com)
+import json
+import random
+import string
+
+import paho.mqtt.client as mqtt
 
 
-class MqttClient(object):
+class Client(object):
+    name = None
+
+
+class MqttClient(Client):
+    name = 'mqtt'
 
     def __init__(self, host, port):
-        host = host
-        port = port
+        self.host = host
+        self.port = port
 
-    def send_data(self, data):
-        pass
+    def generate_number(self):
+        return ''.join(random.sample(string.ascii_letters + string.digits, 16))
+
+    def __enter__(self):
+        self.client = mqtt.Client(client_id=self.generate_number())
+        self.client.connect(host=self.host, port=self.port, keepalive=60)
+        return self
+
+    def send_data(self, topic, data):
+        self.client.publish(topic=topic, payload=data)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(exc_type)
+
+
+if __name__ == '__main__':
+    with MqttClient(host=1111, port=1883) as s:
+        s.send_data()
+        raise Exception
--- work_flow/xiaomi_sensor/model.py
+++ work_flow/xiaomi_sensor/model.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 # © 2016 QYT Technology
 # Authored by: Liu tianlong (tlzmkm@gmail.com)
-from device.xiaomi.models import XiaoMiSensor
+from device.xiaomi.models import DeviceXiaoMiSensor
 from pipeline.mqtt.model import MqttClient
 
 
-class MqttXiaoMiSensor(object):
+class WorkFlow(object):
+    sensor_type = None
+    protocol = None
 
     def __init__(self, name, host, port):
-        self.sensor = XiaoMiSensor(name=name, )
-        self.mqtt_client = MqttClient(host=host, port=port)
+        self.sensor = DeviceSensor.get(sensor=self.sensor_type)
+        self.client = Client.get(self.protocol)
 
     def run(self):
-        data = self.sensor.get_data()
-        self.mqtt_client.send_data(data=data)
+        data = self.sensor
+        self.client.send_data(data=data)
 
+
+class MqttXiaoMiSensor(WorkFlow):
+    sensor_type = 'xiaomi'
+    protocol = 'mqtt'
